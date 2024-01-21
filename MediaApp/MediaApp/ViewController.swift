f6ddc775336d165cbbb26e61e5309b67f83b4b59
Alex Lin Segarra <alexlinsegarra@gmail.com>
2024-01-21 14:17:20 +0100

    Initial commit project mvvm coordinator

---
+++ MediaApp/MediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaApp
+//
+//  Created by Alex Lin Segarra on 21/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
